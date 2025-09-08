78901b8625a45017e6c40a4d678ece4932824105
chendechen <dechen.chen@debank.com>
2025-09-08 20:53:27 +0800

    feat: support trace block

--- arbitrum/apibackend.go
+++ arbitrum/apibackend.go
@@ -185,6 +185,13 @@ func (a *APIBackend) GetAPIs(filterSystem *filters.FilterSystem) []rpc.API {
 		Public:    true,
 	})
 
+	apis = append(apis, rpc.API{
+		Namespace: "trace",
+		Version:   "1.0",
+		Service:   NewDebankAPI(a),
+		Public:    true,
+	})
+
 	apis = append(apis, tracers.APIs(a)...)
 
 	return apis
